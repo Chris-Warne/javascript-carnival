4c2ed2cd1386b1a4e37de33063efe0cc07fd3fae
Chris-Warne <chrisklinx@msn.com>
2022-05-12 16:08:02 +1200

    whack a mole

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,61 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+//create onclick button for each unicorn
+
+let unicorn = document.getElementsByClassName('inflate-an-image')
+
+for (let i = 0; i < unicorn.length; i++) {
+  unicorn[i].onclick = unicornClick
+}
+//changing the unicorn image
+let balloonInflate = 0
+
+function unicornClick(e) {
+  let unicorn = e.target
+
+  if (unicorn.id == 'unicorn-1') {
+    balloonInflate++
+
+    unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(unicorn)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('whoosh')
+      alert('i am full of hot air')
+    }
+  }
+
+  if (unicorn.id == 'unicorn-2') {
+    balloonInflate++
+
+    unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(unicorn)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('whoosh')
+      alert('i am full of hot air')
+    }
+  }
+
+  if (unicorn.id == 'unicorn-3') {
+    balloonInflate++
+
+    unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(unicorn)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('whoosh')
+      alert('i am full of hot air')
+    }
+  }
+  console.log('you clicked me ;-)')
+}
--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,27 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+//store references into cells so that you can put the mole in!
+let cells = document.getElementsByTagName('TD')
+let moleImage = document.createElement('img')
+moleImage.classList.add('mole')
+moleImage.src = 'mole.PNG'
+moleImage.onclick = whackaMole
+
+//create a function that will randomly pop mole in cell
+function putMoleInHole() {
+  let randomIndex = Math.floor(Math.random() * 24)
+  let randomCell = cells[randomIndex]
+
+  randomCell.appendChild(moleImage)
+}
+putMoleInHole()
+function whackaMole() {
+  console.log(putMoleInHole)
+  putMoleInHole()
+  let audio = new Audio()
+  audio.src = `whack-audio.wav`
+  audio.play()
+}
