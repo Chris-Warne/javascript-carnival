cc07953beef1dd72bb8839b06992be379da50753
Chris-Warne <chrisklinx@msn.com>
2022-05-13 16:54:08 +1200

    dress the clown

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,32 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+//console.log('Dress The Clown!')
+
+let headIndex = 0
+
+function changeClothes() {
+  if (headIndex > 5) {
+    headIndex = 0
+  } else if (headIndex < 0) {
+    headIndex = 5
+  }
+}
+
+//make function to change clown head
+function changeClownHead() {
+  let head = document.getElementById('head')
+  head.src = './images/head1.png'
+  let headSrc = './images/head' + headIndex + '.png'
+}
+
+//calling function for arrow key
+document.addEventListener('keydown', function (e) {
+  switch ('right') {
+    case e.key:
+      changeClothes()
+      changeIndexdown()
+      break
+  }
+})
